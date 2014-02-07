34afeaea632d8bab7bbc0d65ba2ebc3d3ba82589
Chris Honiball <chris.p.honiball@gmail.com>
2014-02-06 18:37:20 -0600

    Working for images, need to add other post types now

---
+++ readme.md
@@ -0,0 +1,14 @@
+#Tumblr Backup
+
+A command line tool to download the contents of your Tumblr blog to your computer for safe keeping. Written in Node.
+
+##Downloading
+
+If you have git, do a git clone:
+
+```
+git clone
+```
+
+##To use
+
--- tumblrbackup.js
+++ tumblrbackup.js
@@ -27,6 +27,7 @@ var args = program.parse(process.argv).args;
 var blogUrl = args[0];
 var downloadDir = args[1] || 'downloads/';
 var rename = args[2] || 'image';
+var offset = 0;
 var apiEndpoint = 'http://api.tumblr.com/v2/blog/' + blogUrl + '/posts/photo?api_key=' + api_key;
 
 // add trailing slash if left off download dir
@@ -35,8 +36,9 @@ if (downloadDir[downloadDir.length -1] !== '/') { downloadDir += '/'; }
 // GET request
 if (program.verbose) { console.log('Making request to ' + apiEndpoint); }
 
-function downloadPhotos() {
-	http.get(apiEndpoint, function(res) {
+(function downloadPhotos() {
+	var begin = offset;
+	http.get(apiEndpoint + '&offset=' + offset, function(res) {
 		if (program.verbose) { console.log('Tumblr responded with status ' + res.statusCode); }
 
 		if (res.statusCode == 200) {
@@ -48,27 +50,30 @@ function downloadPhotos() {
 			res.on('end', function() {
 				// note - tumblr returns at most 20 posts in a given query
 				var posts = JSON.parse(result).response.posts;
-				var counter = 0;
 
-				posts.forEach(function(e, i) {
-					// grab URL to the full size photo
-					var photoUrl = e.photos[0].original_size.url;
-					if (program.verbose) { console.log('Downloading photo at URL: ' + photoUrl); }
+				if (posts.length) {
+					posts.forEach(function(e, i) {
+						// grab URL to the full size photo
+						var photoUrl = e.photos[0].original_size.url;
+						if (program.verbose) { console.log('Downloading photo at URL: ' + photoUrl); }
 
-					//get format of photo - tumblr supports only jpg, jpeg, gif, png, bmp
-					var extension = url.parse(photoUrl).pathname.split('.').pop();
+						//get format of photo - tumblr supports only jpg, jpeg, gif, png, bmp
+						var extension = url.parse(photoUrl).pathname.split('.').pop();
 
-					// download photo
-					request(photoUrl).pipe(fs.createWriteStream(downloadDir + rename + '_' + counter + '.' + extension).on('finish', function() {
-						if (program.verbose) { console.log(photoUrl + ' sucessfully downloaded.'); }
-					}));
-					counter++;
-				});
+						// download photo
+						request(photoUrl).pipe(fs.createWriteStream(downloadDir + rename + '_' + offset + '.' + extension).on('finish', function() {
+							if (program.verbose) { console.log(photoUrl + ' sucessfully downloaded.'); }
+						}));
+						offset++;
+						// if the end of the individual photoset is reached, call function again
+						if (offset === begin + 20) {
+							downloadPhotos();
+						}
+					});
+				}
 			});
 		} else {
 			console.log('Error, Tumblr responded with status code ' + res.statusCode);
 		}
 	});
-}
-
-
+})();
